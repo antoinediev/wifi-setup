1ad19d8c93622759639967ad2c92975027ea23de
Thomas <thomaspruvost5959@gmail.com>
2021-04-30 16:37:13 +0200

    fre

--- index.js
+++ index.js
@@ -162,16 +162,14 @@ function getIp() {
         // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
         if (net.family === 'IPv4' && !net.internal) {
           if (!results[name]) {
+            if(name = "wlan0"){
+              ip = net.address;
+            }
             results[name] = [];
           }
           results[name].push(net.address);
-          
         }
       }
-    }
-    console.log(results);
-    if(results.wlan0 !="" && results.wlan0 != null){
-      ip = results.wlan0;
     }
   }
 }
